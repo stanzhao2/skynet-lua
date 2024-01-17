b582ff911a8b3fc1cbcbfb1ddf5b66dd384d2376
stanzhao2 <34759974+stanzhao2@users.noreply.github.com>
2024-01-17 16:51:29 +0800

    update

--- bin/lua/5.4/skynet/coroutine.lua
+++ bin/lua/5.4/skynet/coroutine.lua
@@ -2,20 +2,24 @@
 
 --------------------------------------------------------------------------------
 
-local class = require("skynet.classy");
+local format   = string.format;
+local class    = require("skynet.classy");
 local co_class = class("co_class");
 
 --------------------------------------------------------------------------------
 
-local function print_if_error(ok, err)
-  if ok ~= true then error(err) end
+local function print_if_error(name, ok, err)
+  if ok ~= true then
+    error(format("coroutine(%s) error: ", err));
+  end
 end
 
 --------------------------------------------------------------------------------
 
-function co_class:__init()
+function co_class:__init(name)
   self.tasklist = std.list();
-  self.closed = false;
+  self.name     = name or "unknown";
+  self.closed   = false;
   self.co = coroutine.create(bind(self.comain, self));
 end
 
@@ -45,7 +49,7 @@ function co_class:comain()
       end
       coroutine.yield();
     else
-      print_if_error(pcall(self.tasklist:front()));
+      print_if_error(self.name, pcall(self.tasklist:front()));
       self.tasklist:pop_front();
     end
   end
@@ -56,7 +60,7 @@ end
 
 --------------------------------------------------------------------------------
 
-function co_class:append(task)
+function co_class:dispatch(task)
   assert(type(task) == "function");
   if not self.closed then
     self.tasklist:push_back(task);
